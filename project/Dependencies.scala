8743962e71cdba36a4f36f64c62c5ba93301e0f0
Hanyu Cui <hanyu.cui@databricks.com>
2019-10-15 12:00:47 -0700

    Use local maven

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 object Dependencies {
   // The spark version
   val targetSparkVersion = sys.props.getOrElse("spark.version", "2.4.4")
-  val targetTensorFlowVersion = "1.14.0"
+  val targetTensorFlowVersion = "1.15.0-rc3"
 }
